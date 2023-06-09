c6a8ab380ae36682d3c37a3dca89c9a66662f8c0
SolomonAvraham <Selmon_a@hotmail.com>
2023-06-09 21:37:26 +0300

    Init + models + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "0px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
