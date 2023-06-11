a3f4c8f13c2dc3857ed360fcc62fcc8ec9a436ea
Dawitlior <dawitlior777@gmail.com>
2023-06-11 05:11:23 +0300

    colors settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "wh-10": "#F4F4F4",
+        "wh-50": "#FBFBFB",
+        "wh-100": "#C9C9C9",
+        "wh-300": "#939393",
+        "wh-500": "#595959",
+        "wh-900": "#0F0F0F",
+        "accent-red": "#EA9648",
+        "accent-orange": "#F6CF68",
+        "accent-green": "#C2E9B4",
+      },
       screens: {
         sm: "0px",
         md: "768px",
