8f858521f5cdcd9797afad768b75874b72d4109c
Dawitlior <dawitlior777@gmail.com>
2023-06-11 05:13:22 +0300

    button

--- src/components/features/button/Button.tsx
+++ src/components/features/button/Button.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { ReactNode, MouseEvent } from "react";
 
-export default function Button() {
-  return (
-    <div>Button</div>
-  )
+interface ButtonProps {
+  children: ReactNode;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
 }
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
+  return (
+    <button
+      className={`px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
