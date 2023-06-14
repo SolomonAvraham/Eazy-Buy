e017adae60183afb06ad0fde123c6fd112b2ea7c
Dawitlior <dawitlior777@gmail.com>
2023-06-14 17:28:08 +0300

    middleware

---
+++ server/middleware/index.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (request, response, next) => {
+  try {
+    let token = request.header("Authorization");
+    if (!token) {
+      return response.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    request.user = verified;
+    next();
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+};
