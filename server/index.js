00dad95804fc335eb6ccac00b72be13db8c02eda
Dawitlior <dawitlior777@gmail.com>
2023-06-14 12:01:04 +0300

    server setup and mongoDB

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoute from "./routes/client.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/client", clientRoute);
+
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server Port:${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not Connect`));
