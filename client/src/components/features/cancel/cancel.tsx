62dc42b238ade9437eab8e59a27f8aec6dc5b697
Dawitlior <dawitlior777@gmail.com>
2023-06-25 20:45:00 +0300

    add

--- client/src/components/features/cancel/cancel.tsx
+++ client/src/components/features/cancel/cancel.tsx
@@ -1,8 +1,29 @@
 
+const MyCancelCompo = () => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-transparent text-black-700 px-6 py-8 rounded-lg">
+      <h3 className="text-3xl font-semibold text-center mb-2">Payment Cancelled</h3>
+      <p className="text-lg text-center">
+        We're sorry, but your payment was not successful. Please check your payment information and try again.
+      </p>
+      <div className="mt-4 text-sm text-gray-600">
+        <p>
+          If you continue to experience issues, please contact our support team at
+          <a href="tel:+123456789" className="ml-1 underline">+123456789</a>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+
 
 const Cancel = () => {
   return (
-    <div>Cancel</div>
+    <>
+    <MyCancelCompo />
+    {console.log('cancel')}
+    </>
   )
 
 }
---
+++ client/src/components/pages/about/about.tsx
@@ -0,0 +1,17 @@
+
+const Application = () => {
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <main className="container mx-auto px-4 py-8">
+                <section className="mb-8">
+                    <h2 className="text-2xl font-semibold mb-4">About</h2>
+                    <p className="text-gray-800">
+                        Welcome to My Awesome Application! It is meticulously designed to deliver an unparalleled user experience.
+                    </p>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default Application;
