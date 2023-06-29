7f15dc201d3e2429c319bdfa69d1ba3236f7e934
Dawitlior <dawitlior777@gmail.com>
2023-06-29 18:11:29 +0300

    add

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  deleteUserCart,
   getStripeProducts,
   putStripePurchase,
   updateUserCart,
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/products", getStripeProducts);
 router.post("/create-checkout-session",putStripePurchase);
 router.post("/update", updateUserCart);
+router.delete("/delete", deleteUserCart);
 
 export default router;
