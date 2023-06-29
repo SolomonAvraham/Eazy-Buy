970fcac5ea35176c1e2f8fefa5858d914af4f668
Dawitlior <dawitlior777@gmail.com>
2023-06-29 13:36:12 +0300

    add

--- client/src/components/pages/cart/Cart.tsx
+++ client/src/components/pages/cart/Cart.tsx
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+
 const CartComponent = () => {
+  const { data, isLoading, isError } = useQuery(["products"], getProducts);
+
   const cartItems = [
     { id: 1, name: "Product 1", price: 10 },
     { id: 2, name: "Product 2", price: 15 },
