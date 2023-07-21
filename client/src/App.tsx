9d9aa10fe36f3273814d9ab1d334043ed31c86b4
SolomonAvraham <Selmon_a@hotmail.com>
2023-07-21 21:22:54 +0300

    responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import Success from "./components/features/success/success";
 import Contact from "./components/pages/contact/Contact";
 import CartComponent from "./components/pages/cart/Cart"
 import ShowProduct from "./components/pages/showProuduct/ShowProduct";
-import About from "./components/pages/About/About";
+import About from "./components/pages/about/about";
 import NotFound from "./components/features/notFound";
 
  
--- client/src/components/pages/about/about.tsx
+++ client/src/components/pages/about/about.tsx
@@ -1,3 +1,6 @@
+
+
+
 export default function About(): JSX.Element {
   return (
     <>
